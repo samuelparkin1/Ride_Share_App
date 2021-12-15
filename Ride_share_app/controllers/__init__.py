f093c60f4c070e41e717a1571c524a71652490b7
samuel_parkin <samuelparkin1@gmail.com>
2021-12-15 13:50:45 +1000

    fixed typo

--- Ride_share_app/controllers/__init__.py
+++ Ride_share_app/controllers/__init__.py
@@ -1,5 +1,5 @@
 from controllers.rider_controller import riders
-from controllers.driver_controller import driver
+from controllers.driver_controller import drivers
 from controllers.user_controller import user
 from controllers.image_controller import rider_images
 
